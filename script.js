c9ab7523087b523495388f465e727fd61393ea02
J-herika <138695574+J-Herika@users.noreply.github.com>
2024-04-29 13:18:01 +0300

    made the basic structure work

---
+++ script.js
@@ -0,0 +1,9 @@
+const grid = document.querySelector("#grid");
+
+let gridBox = document.createElement("div");
+
+gridBox.style.backgroundColor = "red";
+gridBox.style.width = "300px";
+gridBox.style.height = "300px";
+
+grid.appendChild(gridBox);
