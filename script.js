887641a4276df9ba80fe8111b86202d32d140b81
J-herika <138695574+J-Herika@users.noreply.github.com>
2024-04-29 16:39:29 +0300

    made the basic mechanics by chaging the color over over the mouse hover

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const grid = document.querySelector("#grid");
+let gridNumber;
 
-
-for (let i = 0; i < 16; i++) {
-
+for (let i = 0; i < 19; i++) {
   let gridBox = document.createElement("div");
   gridBox.className = "grid-box";
 
@@ -11,4 +10,12 @@ for (let i = 0; i < 16; i++) {
   gridBox.style.height = "100px";
 
   grid.appendChild(gridBox);
+}
+const gridBox = document.querySelectorAll(".grid-box");
+console.log(gridBox);
+
+for (let i = 0; i < gridBox.length; i++) {
+  gridBox[i].addEventListener("mouseenter", () => {
+    gridBox[i].style.backgroundColor = "purple";
+  });
 }
